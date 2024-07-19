da269b98a6c6ce61484a64a839ba67e211e94c3d
vv-01 <asvijayavitthala@gmail.com>
2024-07-19 18:22:49 +0530

    changed pool config file to build

--- db.js
+++ db.js
@@ -3,14 +3,14 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.LOCAL_USER,
-  host: process.env.LOCAL_HOST,
-  database: process.env.LOCAL_DATABASE,
-  password: process.env.LOCAL_PASSWORD,
-  port: process.env.LOCAL_PORT,
-  // ssl: {
-  //     rejectUnauthorized: false,
-  //   },
+  user: process.env.CLOUD_USER,
+  host: process.env.CLOUD_HOST,
+  database: process.env.CLOUD_DATABASE,
+  password: process.env.CLOUD_PASSWORD,
+  port: process.env.CLOUD_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
